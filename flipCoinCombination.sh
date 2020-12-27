4e8fb1c7abb17d82f2c96bb3365c540e9ffa4a4e
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-28 00:34:16 +0530

    Add. Added flipping coin code.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,15 @@
 #! /bin/bash -x
 
 echo "***** Welcome to the Flip Coin Combination Script. *****"
+
+heads=1
+tails=0
+
+flipResult=$((RANDOM%2))
+if [ $flipResult -eq 1 ]
+then
+        echo "Heads"
+else
+	echo "Tails"
+fi
+
