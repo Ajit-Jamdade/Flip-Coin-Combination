42a1060ec582ea49d8a491bcba033be6b187fbb2
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-28 21:54:15 +0530

    Add. Added dictionary to store singlet combination.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -7,13 +7,19 @@ tails=0
 
 read -p "How many times you want to flip the coin ? " flipCount
 
+declare -A dict
+
 for (( i=1; i<=$flipCount; i++ ))
 do
-	flipResult=$((RANDOM%2))
-	if [ $flipResult -eq 1 ]
-	then
-		echo "H"
-	else
-		echo "T"
-	fi
+        flipResult=$((RANDOM%2))
+        if [ $flipResult -eq 1 ]
+        then
+                echo "H"
+                dict[$i]="H"
+        else
+                echo "T"
+                dict[$i]="T"
+        fi
 done
+
+echo ${dict[@]}
