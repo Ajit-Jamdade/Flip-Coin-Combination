96f2e7d7a88e504df337f7a5d3bbb4ee93c1a1fa
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-28 21:40:51 +0530

    Add. Added displaying coin flipping results.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,11 +5,15 @@ echo "***** Welcome to the Flip Coin Combination Script. *****"
 heads=1
 tails=0
 
-flipResult=$((RANDOM%2))
-if [ $flipResult -eq 1 ]
-then
-        echo "Heads"
-else
-	echo "Tails"
-fi
+read -p "How many times you want to flip the coin ? " flipCount
 
+for (( i=1; i<=$flipCount; i++ ))
+do
+	flipResult=$((RANDOM%2))
+	if [ $flipResult -eq 1 ]
+	then
+		echo "H"
+	else
+		echo "T"
+	fi
+done
