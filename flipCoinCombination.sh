bd826f47a6826d43c75450da7a47e84d39404f70
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-28 22:26:31 +0530

    Add. Added Doublet Combination and store it into dictionary.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,24 +2,27 @@
 
 echo "***** Welcome to the Flip Coin Combination Script. *****"
 
-heads=1
-tails=0
-
-read -p "How many times you want to flip the coin ? " flipCount
+read -p "How many times you want to flip two coins ? " flipCount
 
 declare -A dict
 
 for (( i=1; i<=$flipCount; i++ ))
 do
-        flipResult=$((RANDOM%2))
-        if [ $flipResult -eq 1 ]
-        then
-                echo "H"
-                dict[$i]="H"
-        else
-                echo "T"
-                dict[$i]="T"
-        fi
+        flipResult=$((RANDOM%4))
+	case $flipResult in
+		0)
+                	dict[$i]="HH"
+			;;
+		1)
+                	dict[$i]="HT"
+			;;
+		2)
+			dict[$i]="TH"
+			;;
+		3)
+			dict[$i]="TT"
+			;;
+	esac
 done
 
 echo ${dict[@]}
